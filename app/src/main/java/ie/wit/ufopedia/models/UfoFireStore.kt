8d42f88a85364d2c9d652e5afa67d5142aad1ab8
Arunasjok <jokubynas@gmail.com>
2022-12-23 10:38:57 +0000

    Firebase storage finish.

--- app/src/main/java/ie/wit/ufopedia/models/UfoFireStore.kt
+++ app/src/main/java/ie/wit/ufopedia/models/UfoFireStore.kt
@@ -1,14 +1,21 @@
 package ie.wit.ufopedia.models
 
 import android.content.Context
+import android.graphics.Bitmap
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import ie.wit.ufopedia.helpers.readImageFromPath
+import timber.log.Timber.i
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class UfoFireStore(val context: Context) : UfoStore {
     val ufos = ArrayList<UfoModel>()
     lateinit var userId: String
     lateinit var db: DatabaseReference
-
+    lateinit var st: StorageReference
     override suspend fun findAll(): List<UfoModel> {
         return ufos
     }
@@ -24,6 +31,7 @@ class UfoFireStore(val context: Context) : UfoStore {
             ufo.fbId = key
             ufos.add(ufo)
             db.child("users").child(userId).child("ufos").child(key).setValue(ufo)
+            updateImage(ufo)
         }
     }
 
@@ -37,7 +45,9 @@ class UfoFireStore(val context: Context) : UfoStore {
         }
 
         db.child("users").child(userId).child("ufos").child(ufo.fbId).setValue(ufo)
-
+        if(ufo.image.length > 0){
+            updateImage(ufo)
+        }
     }
 
     override suspend fun delete(ufo: UfoModel) {
@@ -64,9 +74,38 @@ class UfoFireStore(val context: Context) : UfoStore {
             }
         }
         userId = FirebaseAuth.getInstance().currentUser!!.uid
+        st = FirebaseStorage.getInstance().reference
         db = FirebaseDatabase.getInstance().reference
         ufos.clear()
         db.child("users").child(userId).child("ufos")
             .addListenerForSingleValueEvent(valueEventListener)
+    }
+    fun updateImage(ufo: UfoModel){
+        if(ufo.image != ""){
+            val fileName = File(ufo.image)
+            val imageName = fileName.getName()
+
+            var imageRef = st.child(userId + '/' + imageName)
+            val baos = ByteArrayOutputStream()
+            val bitmap = readImageFromPath(context, ufo.image)
+
+            bitmap?.let {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos)
+
+                val data = baos.toByteArray()
+                val uploadTask = imageRef.putBytes(data)
+
+                uploadTask.addOnSuccessListener { taskSnapshot ->
+                    taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+                        ufo.image = it.toString()
+                        db.child("users").child(userId).child("ufos").child(ufo.fbId).setValue(ufo)
+                    }
+                }.addOnFailureListener{
+                    var errorMessage = it.message
+                    i("Failure: $errorMessage")
+                }
+            }
+
+        }
     }
 }
